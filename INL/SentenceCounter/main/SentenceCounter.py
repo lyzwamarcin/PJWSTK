f881349196a494e338d1350cff75f717ea834154
lyzwamarcin <marcin.lyzwa@gmail.com>
2015-03-08 16:48:53 +0100

    Sentence Counter project.

--- INL/SentenceCounter/main/SentenceCounter.py
+++ INL/SentenceCounter/main/SentenceCounter.py
@@ -1,2 +1,14 @@
-print "Marcin"
-print "Test"
+import os.path
+
+__SHORTCUTS_FILE_PATH__ = '..\data\shortcuts.txt'
+
+
+def read_shortcuts():
+    if not os.path.exists(__SHORTCUTS_FILE_PATH__):
+        raise IOError('File ' + __SHORTCUTS_FILE_PATH__ + 'not found!')
+
+    return open(__SHORTCUTS_FILE_PATH__, 'r')
+
+
+def __main__():
+    print ''
